0054c4c7a69ef8e1b0c163b49ce0ed3f8ed3f797
jamesdongdong <ddx95668518@gmail.com>
2024-03-15 01:23:29 +0900

    基本搜索功能

--- extension.js
+++ extension.js
@@ -12,16 +12,36 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vue-find-father-compoent" is now active!');
+	// console.log('Congratulations, your extension "vue-find-father-compoent" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('vue-find-father-compoent.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vue-find-father-compoent!');
+		// get current file name without suffix
+		const editor = vscode.window.activeTextEditor;
+		const fileSuffix = editor.document.fileName.split('.').pop();
+		const fileName = editor.document.fileName.split('/').pop().split('.')[0];
+
+		if (fileSuffix !== 'vue') {
+			vscode.window.showErrorMessage('is not a vue file!');
+			return;
+		}
+		
+		// In the global search, search for '<fileName'
+		vscode.commands.executeCommand('workbench.action.findInFiles', {
+			query: `<${fileName}`,
+			triggerSearch: true,
+			matchWholeWord: true,
+			isCaseSensitive: true,
+			isRegex: false,
+			filesToInclude: '',
+			filesToExclude: '',
+			onlyOpenEditors: false,
+			isSmartCase: true,
+			preserveCase: true
+		});
 	});
 
 	context.subscriptions.push(disposable);
